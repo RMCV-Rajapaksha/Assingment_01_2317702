8eae7ff1b5564ce6efc9601984a9aed1f10340cf
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-04-16 23:22:48 +0530

    Add hero.dart file and update main.dart

--- Flutter_animatioon/flutter_animation/lib/main.dart
+++ Flutter_animatioon/flutter_animation/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_animation/Implicit.dart';
 import 'package:flutter_animation/PhysicsBasedAnimation.dart';
 import 'package:flutter_animation/explicit.dart';
+import 'package:flutter_animation/hero.dart';
 import 'package:flutter_animation/tween.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: PhysicsAnimationDemo(),
+          child: Screen1(),
         ),
       ),
     );
