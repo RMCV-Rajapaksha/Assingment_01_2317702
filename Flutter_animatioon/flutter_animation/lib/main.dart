408c41b337c74a0e6c5f40aa0acb432b152112c2
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-04-16 23:12:33 +0530

    Add new animation demos and update dependencies

--- Flutter_animatioon/flutter_animation/lib/main.dart
+++ Flutter_animatioon/flutter_animation/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animation/Implicit.dart';
+import 'package:flutter_animation/PhysicsBasedAnimation.dart';
+import 'package:flutter_animation/explicit.dart';
+import 'package:flutter_animation/tween.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,10 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: FadeInDemo(),
+          child: PhysicsAnimationDemo(),
         ),
       ),
     );
